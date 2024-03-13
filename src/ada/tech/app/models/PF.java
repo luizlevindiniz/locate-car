18f8882b6867cbe203e7655f7c7ba8a396c6fbc9
ryangois <ryangoisdev@gmail.com>
2024-03-13 19:26:16 -0300

    implementação da validação de CPF

--- src/ada/tech/app/models/PF.java
+++ src/ada/tech/app/models/PF.java
@@ -10,9 +10,21 @@ import lombok.Setter;
 @Getter
 public class PF extends Pessoa {
     private String CPF;
+    public void setCPF(String CPF) {
+        CPF = CPF.replaceAll("[^0-9]", "");
+
+        if (validarCPF(CPF)) {
+            this.CPF = CPF;
+        } else {
+            throw new IllegalArgumentException("CPF inválido");
+        }
+    }
+    private boolean validarCPF(String CPF) {
+        return CPF.matches("\\d{11}") && !CPF.matches("(\\d)\\1{10}");
+    }
     public PF(String nome, String CPF, boolean alugouCarro) {
         super( nome, alugouCarro);
-        this.CPF = CPF;
+        setCPF(CPF);
     }
 
     @Override
