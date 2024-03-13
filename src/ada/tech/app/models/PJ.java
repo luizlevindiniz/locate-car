e6f3385d4331fb81e1f4a3bcd8ae1a8d09bb38ba
ryangois <ryangoisdev@gmail.com>
2024-03-13 19:27:46 -0300

    implementação da validação de CNPJ

--- src/ada/tech/app/models/PJ.java
+++ src/ada/tech/app/models/PJ.java
@@ -8,9 +8,22 @@ import lombok.*;
 public class PJ extends Pessoa{
     private String CNPJ;
 
+    public void setCNPJ(String CNPJ) {
+        CNPJ = CNPJ.replaceAll("[^0-9]", "");
+
+        if (validarCNPJ(CNPJ)) {
+            this.CNPJ = CNPJ;
+        } else {
+            throw new IllegalArgumentException("CNPJ inválido");
+        }
+    }
+
+    private boolean validarCNPJ(String CNPJ) {
+        return CNPJ.matches("\\d{14}") && !CNPJ.matches("(\\d)\\1{13}");
+    }
     public PJ(String nome, String CNPJ, boolean alugouCarro) {
         super( nome, alugouCarro);
-        this.CNPJ = CNPJ;
+        setCNPJ(CNPJ);
     }
 
     @Override
