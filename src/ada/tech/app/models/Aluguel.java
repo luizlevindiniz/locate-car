c5fa6498a0bb522c5cd34eaea4ee074ba4ba4c1a
ryangois <ryangoisdev@gmail.com>
2024-03-13 19:47:09 -0300

    criação da classe aluguel e regras de negócio

---
+++ src/ada/tech/app/models/Aluguel.java
@@ -0,0 +1,36 @@
+package ada.tech.app.models;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Aluguel {
+    private Pessoa pessoa;
+    private Veiculo veiculo;
+    private LocalDate dataInicio;
+    private int duracaoEmDias;
+
+
+    public double calcularValorTotal() {
+        boolean isSUV = veiculo.getTipo() == TipoCarro.SUV;
+
+        double valorDiaria = isSUV ? 200.0 : (veiculo.getTipo() == TipoCarro.MEDIO ? 150.0 : 100.0);
+
+        double valorTotal = valorDiaria * duracaoEmDias;
+
+        if (pessoa instanceof PF && duracaoEmDias > 5) {
+            valorTotal *= 0.95;
+        } else if (pessoa instanceof PJ && duracaoEmDias > 3) {
+            valorTotal *= 0.90;
+        }
+
+        return valorTotal;
+    }
+
+}
+
