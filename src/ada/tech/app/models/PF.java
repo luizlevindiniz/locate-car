8a27da3d615ec0224a374da7004564c4d869d044
ryangois <ryangoisdev@gmail.com>
2024-03-13 21:11:44 -0300

    melhoramento na validação de CPF

--- src/ada/tech/app/models/PF.java
+++ src/ada/tech/app/models/PF.java
@@ -20,7 +20,34 @@ public class PF extends Pessoa {
         }
     }
     private boolean validarCPF(String CPF) {
-        return CPF.matches("\\d{11}") && !CPF.matches("(\\d)\\1{10}");
+            CPF = CPF.replaceAll("[^0-9]", "");
+
+
+            if (CPF.length() != 11)
+                return false;
+
+            if (CPF.matches("(\\d)\\1{10}"))
+                return false;
+
+            int soma = 0;
+            for (int i = 0; i < 9; i++) {
+                soma += (CPF.charAt(i) - '0') * (10 - i);
+            }
+            int resto = soma % 11;
+            int digitoVerificador1 = resto < 2 ? 0 : 11 - resto;
+
+            if ((CPF.charAt(9) - '0') != digitoVerificador1)
+                return false;
+
+            soma = 0;
+            for (int i = 0; i < 10; i++) {
+                soma += (CPF.charAt(i) - '0') * (11 - i);
+            }
+            resto = soma % 11;
+            int digitoVerificador2 = resto < 2 ? 0 : 11 - resto;
+
+            return (CPF.charAt(10) - '0') == digitoVerificador2;
+        }
     }
     public PF(String nome, String CPF, boolean alugouCarro) {
         super( nome, alugouCarro);
