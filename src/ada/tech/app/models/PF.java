998e956c9c41b98b5995f125171b3f283a5b3b30
luizlevindiniz <luizd97@gmail.com>
2024-03-13 21:21:21 -0300

    implementa as RNs de aluguel

--- src/ada/tech/app/models/PF.java
+++ src/ada/tech/app/models/PF.java
@@ -1,7 +1,6 @@
 package ada.tech.app.models;
 
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +9,12 @@ import lombok.Setter;
 @Getter
 public class PF extends Pessoa {
     private String CPF;
+
+    public PF(String nome, String CPF, boolean alugouCarro) {
+        super(nome, alugouCarro);
+        setCPF(CPF);
+    }
+
     public void setCPF(String CPF) {
         CPF = CPF.replaceAll("[^0-9]", "");
 
@@ -19,16 +24,39 @@ public class PF extends Pessoa {
             throw new IllegalArgumentException("CPF inválido");
         }
     }
+
     private boolean validarCPF(String CPF) {
-        return CPF.matches("\\d{11}") && !CPF.matches("(\\d)\\1{10}");
-    }
-    public PF(String nome, String CPF, boolean alugouCarro) {
-        super( nome, alugouCarro);
-        setCPF(CPF);
+        CPF = CPF.replaceAll("[^0-9]", "");
+
+
+        if (CPF.length() != 11)
+            return false;
+
+        if (CPF.matches("(\\d)\\1{10}"))
+            return false;
+
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += (CPF.charAt(i) - '0') * (10 - i);
+        }
+        int resto = soma % 11;
+        int digitoVerificador1 = resto < 2 ? 0 : 11 - resto;
+
+        if ((CPF.charAt(9) - '0') != digitoVerificador1)
+            return false;
+
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += (CPF.charAt(i) - '0') * (11 - i);
+        }
+        resto = soma % 11;
+        int digitoVerificador2 = resto < 2 ? 0 : 11 - resto;
+
+        return (CPF.charAt(10) - '0') == digitoVerificador2;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return this.nome + " " + this.CPF;
     }
 
